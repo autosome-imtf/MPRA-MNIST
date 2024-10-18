2dc5033f734ad8edd2c07050cba6324f9e389e06
Nikita P <nios.583@gmail.com>
2024-10-18 11:53:03 +0300

    added examples

--- dataset.py
+++ dataset.py
@@ -10,13 +10,8 @@ from dataclass import SeqObj, VectorDsFeature, ScalarDsFeature
 class MpraDataset(Dataset):
     
     """ Sequence dataset. """
-
-    flag = ...
-    
-    def __init__(self,
-                 split,
-                 cell_type: str = "",
-                 transform = None):
+    Parent_flag = "MpraDaraset"
+    def __init__(self, split, cell_type = "", transform = None):
         """
         Parameters
         ----------
@@ -30,9 +25,9 @@ class MpraDataset(Dataset):
         self._scalars = {}
         self._vectors = {}
         self._data_path = "./datasets/" + self.flag + "/"
-
+        self._cell_type = cell_type
         self.split = split
-        self.ds = self.splitting(self._data_path, self.split, cell_type)
+        self.ds = self.splitting(self._data_path, self.split, self._cell_type)
         
         
     def __getitem__(self, i):
@@ -54,7 +49,7 @@ class MpraDataset(Dataset):
         split_default = {"train" : [0,8], "val" : [9], "test" : [10]}
         dataframe = pd.read_csv(data_path + cell_type + '.tsv', sep='\t')
         
-        if isinstance(split,str):
+        if isinstance(split,str): # if split is str and "train", "val" or "test"
             if split in split_default:
                 if len(split_default[split]) == 1:
                     df = dataframe[dataframe.fold == split_default[split][0]]
@@ -62,21 +57,15 @@ class MpraDataset(Dataset):
                     df = dataframe[(dataframe.fold >= 0) & (dataframe.fold <= 8)]
             else:
                 raise ValueError
-        elif isinstance(split, list):
+        elif isinstance(split, list): # if split is a list
             for spl in split:
                 assert spl < 11 and spl > 0, f"{spl} not in range 1 - 10" 
-            if len(split) == 2:
-                df = dataframe[(dataframe.fold >= split[0]) & (dataframe.fold <= split[1])]
-            elif len(split) == 1:
-                df = dataframe[dataframe.fold == split[0]]
-            else:
-                raise ValueError
-        elif isinstance(split, int):
+            df = dataframe[dataframe.fold.isin(split)].reset_index(drop = True)
+        elif isinstance(split, int): # if split is integer
             assert split < 11 and split > 0, f"{split} not in range 1 - 10" 
             df = dataframe[dataframe.fold == split]
         else:
             raise ValueError
-                
         return df
     @property
     def scalars(self):
@@ -101,6 +90,31 @@ class MpraDataset(Dataset):
     def __len__(self):
         return len(self.ds.seq)
 
+    def __repr__(self):
+        """Adapted from torchvision."""
+        _repr_indent = 4
+        head = f"Dataset {self.flag} of size {self.__len__()} ({self.Parent_flag})"
+        body = [f"Number of datapoints: {self.__len__()}"]
+        body.append(f"Split: {self.split}")
+        body.append(f"Sequence size: {len(self.__getitem__(0)[0][0])}")
+        body.append(f"Number of channels: {len(self.__getitem__(0)[0])}")
+        lines = [head] + [" " * _repr_indent + line for line in body]
+        return "\n".join(lines)
+
+"""
+        DATASETS
+"""
+
 class VikramDataset(MpraDataset):
+    """
+        Parameters
+        ----------
+        cell_type (string): "HepG2" is a human liver cancer cell line, "K562" is myelogenous leukemia cell line, "WTC11" is pluripotent stem cell line derived from adult skin 
+        """
     flag = "VikramDataset"
+    def __init__(self, split, cell_type, transform = None):
+        super().__init__(split, cell_type, transform)
+
+
+        
         
