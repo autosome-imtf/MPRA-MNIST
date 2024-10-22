8579021e9d0dbfbb5fbd88a5b337be3c8843b5f8
Nikita P <nios.583@gmail.com>
2024-10-22 14:53:09 +0300

    fixed PearsonCorr counting

--- test_predict.py
+++ test_predict.py
@@ -8,15 +8,18 @@ from pathlib import Path
 import scipy
 from scipy import stats
 from training_config import TrainingConfig
-
+def get_name(full_name: str) -> str:
+        return full_name.split("_", maxsplit=1)[0]
 
 def save_predict(trainer: pl.Trainer,
                  model: pl.LightningModule, 
                  data: SeqDataModule,
                  fold: int,
                  cfg: TrainingConfig,
+                 save_dir: Path,
                  to_mean_score = False,
-                 data_path_averaged = "datasets/VikramDataset/HepG2_averaged.tsv"
+                 data_path_averaged = "datasets/VikramDataset/HepG2_averaged.tsv",
+                 pref: str = "",
 ):
     df_mean = pd.read_csv(data_path_averaged, 
                  sep='\t')
@@ -43,9 +46,27 @@ def save_predict(trainer: pl.Trainer,
                                        dataloaders=dl)
     if cfg.reverse_augment and to_mean_score:
         print("====================")
-        print("с усреднением скора")
+        print("с усреднением скора по аугментациям")
         print("====================")
-        print(stats.pearsonr(df_mean.mean_value,df.mean(axis = 1)))
+        df_mean['pred'] = ((df['forw_pred'] + df['rev_pred']) / 2).values
+        print(stats.pearsonr(df_mean.mean_value,df_mean.pred))
+        
+        print("====================")
+        print("с усреднением скора по цепям")
+        print("====================")
+        names = df_mean['seq_id'].apply(lambda x: get_name(x))
+        sm = df_mean.groupby(names).mean(numeric_only=True)
+        print(stats.pearsonr(sm['pred'], sm['mean_value']))
     else:
         pass
+    if pref != "":
+        df.to_csv(save_dir / f"predictions_{pref}.tsv", 
+                  sep='\t', 
+                  index=False)
+    else:
+        df.to_csv(save_dir / f"predictions.tsv", 
+                  sep='\t', 
+                  index=False)
     return None
+    
+    
