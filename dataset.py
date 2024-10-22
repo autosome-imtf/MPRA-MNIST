3cf86bb1a8cdf8a0a95948d1d32160c6b36f9456
Nikita P <nios.583@gmail.com>
2024-10-22 14:48:11 +0300

    fixed split_parse()

--- dataset.py
+++ dataset.py
@@ -4,31 +4,30 @@ from typing import List, T
 import torch
 
 from torch.utils.data import  Dataset
-from utils import splitting
 from dataclass import SeqObj, VectorDsFeature, ScalarDsFeature
 
 class MpraDataset(Dataset):
-    cell_types = ['HepG2', 'K562', 'WTC11']
-    """ Sequence dataset. """
+    
+    """ SEQUENCE DATASET. """
+    
     Parent_flag = "MpraDaraset"
-    def __init__(self, split, cell_type = "", transform = None):
+    split: str
+    cell_type: str
+    transform = None
+    
+    def __init__(self):
         """
         Parameters
         ----------
-        split (string): 'train', 'val' or 'test', required
+        split (string)
         cell_type (string)
         transform (callable, optional): A function/transform that takes in NuclSeq dataclass object and returns a transformed version. Default: None.
         """
         #self.info = INFO[self.flag] # cool method to info about dataset
         
-        self.transform = transform
         self._scalars = {}
         self._vectors = {}
-        self._data_path = "./datasets/" + self.flag + "/"
-        self._cell_type = cell_type
-        self.split = split
-        self.ds = splitting(self._data_path, self.split, self._cell_type)
-        
+        self._data_path = "./datasets/" + self.flag + "/"        
         
     def __getitem__(self, i):
         sequence = self.ds.seq.values[i]
@@ -74,7 +73,7 @@ class MpraDataset(Dataset):
         _repr_indent = 4
         head = f"Dataset {self.__class__.__name__} of size {self.__len__()} ({self.Parent_flag})"
         body = [f"Number of datapoints: {self.__len__()}"]
-        body.append(f"Split: {self.split}")
+        body.append(f"Split fold: {self.split}")
         body.append(f"Sequence size: {len(self.__getitem__(0)[0][0])}")
         body.append(f"Number of channels: {len(self.__getitem__(0)[0])}")
         lines = [head] + [" " * _repr_indent + line for line in body]
@@ -88,21 +87,34 @@ class VikramDataset(MpraDataset):
     """
         Parameters
         ----------
+        split (string): 'train', 'val' or 'test', required
         cell_type (string): "HepG2" is a human liver cancer cell line, "K562" is myelogenous leukemia cell line, "WTC11" is pluripotent stem cell line derived from adult skin 
     """
+    cell_types = ['HepG2', 'K562', 'WTC11']
     flag = "VikramDataset"
+    
     def __init__(self, split, cell_type, transform = None):
-        super().__init__(split, cell_type, transform)
+        super().__init__()
         
-class VikramTestDataset(MpraDataset):
-    """
-        Parameters
-        ----------
-        cell_type (string): "HepG2" is a human liver cancer cell line, "K562" is myelogenous leukemia cell line, "WTC11" is pluripotent stem cell line derived from adult skin 
-    """
-    flag = "VikramDataset"
-    def __init__(self, split, cell_type, transform = None):
-        super().__init__(split, cell_type + "_averaged", transform)
-
+        self._cell_type = cell_type
+        self.transform = transform
+        self.split = self.split_parse(split)
         
+        df = pd.read_csv(self._data_path + self._cell_type + '.tsv', sep='\t')
+        self.ds = df[df.fold.isin(self.split)]
         
+    def split_parse(self, split: list[int] | int | str) -> list[int]:
+        
+        split_default = {"train" : [1, 2, 3, 4, 5, 6, 7, 8], "val" : [9], "test" : [10]} # default split of data
+        
+        if isinstance(split, str):
+            assert split in split_default.keys(), "Non-existing split value of data" # check if string correct
+            split = split_default[split]
+        if isinstance(split, list):
+            for spl in split:
+                assert spl < 11 and spl > 0, f"{spl} not in range 1 - 10" # check if list correct
+        if isinstance(split, int):
+            assert split < 11 and split > 0, f"{split} not in range 1 - 10"  # check if integer correct
+            split = [split]
+        
+        return split
