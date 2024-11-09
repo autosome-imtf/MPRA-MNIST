2def2509b340098f4aaa0e6a8e6b0ef88f3cfdb6
Nikita P <nios.583@gmail.com>
2024-11-09 18:33:17 +0300

    updated file, deleted vikram and malinois from it

--- mpradataset.py
+++ mpradataset.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
-from typing import List, T, Union
+from typing import List, T, Union, Optional, Callable
 import torch
-from functools import partial
 
 from torch.utils.data import  Dataset
 from dataclass import SeqObj, VectorDsFeature, ScalarDsFeature
@@ -31,12 +30,12 @@ class MpraDataset(Dataset):
     
     
     def __init__(self,
-                 split: str | List[int] | int,
-                 cell_type: str = None,
+                 split: str | List[int] | int | List[Union[int, str]],
+                 cell_type: str | List[str] = None,
                  download: bool = False,
                  root: str = DEFAULT_ROOT,
-                 transform = None,
-                 target_transform = None
+                 transform: Optional[Callable] = None,
+                  target_transform: Optional[Callable] = None
                 ):
         self.split = split
         self._cell_type = cell_type
@@ -50,7 +49,7 @@ class MpraDataset(Dataset):
     def __getitem__(self, i):
         
         sequence = self.ds.seq.values[i]
-        target = self.ds.target.values[i].astype(np.float32)
+        target = self.ds[self.target].values[i].astype(np.float32)
         
         scals = {name: sc[i] for name, sc in self.scalars.items()} if hasattr(self, 'scalars') else {}
         vecs = {name: vec[i] for name, vec in self.vectors.items()} if hasattr(self, 'vectors') else {}
@@ -106,251 +105,3 @@ class MpraDataset(Dataset):
         lines = [head] + [" " * _repr_indent + line for line in body]
         return "\n".join(lines)
         
-"""
-        DATASETS
-"""
-
-class VikramDataset(MpraDataset):
-    """
-        Attributes
-        ----------
-        split : str | List[int] | int
-            Defines which split to use (e.g., 'train', 'val', 'test', or list of fold indices).
-        cell_type : str
-            Specifies the cell type for filtering the data.
-        averaged_target : bool
-            Use target column with averaged expression or not
-        transform : callable, optional
-            Transformation applied to each sequence object.
-        target_transform : callable, optional
-            Transformation applied to the target data.
-    """
-    cell_types = ['HepG2', 'K562', 'WTC11']
-    flag = "VikramDataset"
-    
-    def __init__(self,
-                 split: str | List[int] | int,
-                 cell_type: str,
-                 averaged_target: bool = False,
-                 transform = None,
-                 target_transform = None,
-                ):
-        super().__init__(split, cell_type)
-        if cell_type not in self.cell_types:
-            raise ValueError(f"Invalid cell_type: {cell_type}. Must be one of {self.cell_types}.")
-        self._cell_type = cell_type
-        self.transform = transform
-        self.target_transform = target_transform
-        self.split = self.split_parse(split)
-        
-        try:
-            df = pd.read_csv(self._data_path + self._cell_type + '.tsv', sep='\t')
-        except FileNotFoundError:
-            raise FileNotFoundError(f"File not found: {self._data_path + self._cell_type}.tsv")
-            
-        target_column = "averaged_expression" if averaged_target else "expression"
-        df["target"] = df[target_column].astype(np.float32)
-            
-        self.ds = df[df.fold.isin(self.split)].reset_index(drop=True)
-        
-    def split_parse(self, split: list[int] | int | str) -> list[int]:
-        '''
-        Parses the input split and returns a list of folds.
-        '''
-        
-        split_default = {"train" : [1, 2, 3, 4, 5, 6, 7, 8], 
-                         "val" : [9], 
-                         "test" : [10]
-                        } # default split of data
-        
-        # Process string input
-        if isinstance(split, str):
-            if split not in split_default:
-                raise ValueError(f"Invalid split value: {split}. Expected 'train', 'val', or 'test'.")
-            split = split_default[split]
-        
-        # int to list for unified processing
-        if isinstance(split, int):
-            split = [split]
-            
-        # Check the range of values for a list
-        if isinstance(split, list):
-            for spl in split:
-                if not (1 <= spl <= 10):
-                    raise ValueError(f"Fold {spl} not in range 1-10.")
-        
-        return split
-
-class MalinoisDataset(MpraDataset):
-    """
-        Attributes
-        ----------
-        split : str | List[int] | int
-            Defines which split to use (e.g., 'train', 'val', 'test', or list of fold indices).
-        cell_type : str
-            Specifies the cell type for filtering the data.
-        
-        transform : callable, optional
-            Transformation applied to each sequence object.
-        target_transform : callable, optional
-            Transformation applied to the target data.
-    """
-    cell_types = ['HepG2', 'K562', 'SKNSH']
-    data_project = ['UKBB', 'GTEX', 'CRE']
-    project_column = 'data_project'
-    sequence_column = 'sequence'
-    chr_column = 'chr'
-    stderr_columns = ['K562_lfcSE', 'HepG2_lfcSE', 'SKNSH_lfcSE']
-    flag = "MalinoisDataset"
-    
-    def __init__(self,
-                 split: str | List[Union[int, str]] | int,
-                 cell_type: str,
-                 activity_columns = ['K562_log2FC', 'HepG2_log2FC', 'SKNSH_log2FC'],
-                 normalize: bool = False,
-                 duplication_cutoff: float = 0.5,
-                 stderr_threshold: float = 1.0,
-                 std_multiple_cut: float = 6.0,
-                 up_cutoff_move: float = 4.0,
-                 padded_seq_len: int = 600, 
-                 synth_seed: int = 0,
-                 transform = None,
-                 target_transform = None,
-                ):
-        super().__init__(split, cell_type)
-        
-        if cell_type not in self.cell_types:
-            raise ValueError(f"Invalid cell_type: {cell_type}. Must be one of {self.cell_types}.")
-        self.activity_columns = activity_columns
-        self.normalize = normalize
-        self.duplication_cutoff = duplication_cutoff
-        self.stderr_threshold = stderr_threshold
-        self.std_multiple_cut = std_multiple_cut
-        self.up_cutoff_move = up_cutoff_move
-        self.padded_seq_len = padded_seq_len
-        self._cell_type = cell_type
-        self.transform = transform
-        self.target_transform = target_transform
-
-        self.split = self.split_parse(split)
-        self.ds = self._load_and_filter_data()
-
-    def _load_and_filter_data(self):
-        """
-        Preprocesses the dataset based on provided parameters.
-        """
-        columns = [self.sequence_column, *self.activity_columns, self.chr_column, self.project_column, *self.stderr_columns]
-        try:
-            df = pd.read_csv(self._data_path + 'Malinois.tsv', sep='\t', low_memory=False)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"File not found: {self._data_path}.tsv")
-
-        df = df[columns].dropna()
-        
-        # set name of sequence column to seq
-        df.rename(columns = {'sequence': 'seq'}, inplace = True) 
-        self.sequence_column = "seq"
-        
-        # define target_column
-        target_column = self._cell_type + "_log2FC" 
-        df["target"] = df[target_column].astype(np.float32) 
-        
-        df = df[df[self.project_column].isin(self.data_project)].reset_index(drop=True)
-
-        # filter by stderr
-        # Threshold for feature stderr of examples to be used for train/val/test
-        quality_filter = df[self.stderr_columns].max(axis = 1) < self.stderr_threshold 
-        df = df[quality_filter].reset_index(drop=True)
-
-        # Cut-off for extreme value filtering
-        means = df[self.activity_columns].mean().to_numpy()
-        stds  = df[self.activity_columns].std().to_numpy()
-
-        # Cut-off shift for extreme value filtering
-        up_cut   = means + stds * self.std_multiple_cut + self.up_cutoff_move
-        down_cut = means - stds * self.std_multiple_cut 
-        
-        non_extremes_filter_up = (df[self.activity_columns] < up_cut).to_numpy().all(axis=1)
-        df = df.loc[non_extremes_filter_up]
-        
-        non_extremes_filter_down = (df[self.activity_columns] > down_cut).to_numpy().all(axis=1)
-        df = df.loc[non_extremes_filter_down]
-        self.num_examples = len(df)
-        if self.normalize:   
-            df[self.activity_columns] = (df[self.activity_columns] - means) / stds
-            self.activity_means = torch.Tensor(means)
-            self.activity_stds = torch.Tensor(stds)
-
-        for idx, cell in enumerate(self.activity_columns):
-            cell_name = cell.rstrip('_mean')
-            top_cut_value = round(up_cut[idx], 2)
-            bottom_cut_value = round(down_cut[idx], 2)
-            print(f'{cell_name} | top cut value: {top_cut_value}, bottom cut value: {bottom_cut_value}')
-        print('')    
-        num_up_cuts   = np.sum(~non_extremes_filter_up)
-        num_down_cuts = np.sum(~non_extremes_filter_down)
-        print(f'Number of examples discarded from top: {num_up_cuts}')
-        print(f'Number of examples discarded from bottom: {num_down_cuts}')
-        print('')
-        print(f'Number of examples available: {self.num_examples}')
-        print('-'*50)
-        print('')
-
-        df = df[df.chr.isin(self.split)].reset_index(drop=True)
-        
-        activities = df[self.activity_columns].to_numpy()
-        activity_tensor = torch.Tensor(activities)
-        sort_tensor = torch.max(activity_tensor, dim = -1).values
-
-        #duplicating
-        self.n_duplicated = 0
-        if self.duplication_cutoff is not None:
-            
-            self.n_duplicated = (sort_tensor >= self.duplication_cutoff).sum().item()
-            print(df.seq.shape[0])
-            print(self.n_duplicated)
-        return df
-
-    def __len__(self):
-        #dataset_len = self.ds.seq.shape[0] + self.n_duplicated
-        dataset_len = self.ds.seq.shape[0]
-        return dataset_len
-
-
-    def split_parse(self, split: list[Union[int, str]] | int | str) -> list[str]:
-        '''
-        Parses the input split and returns a list of folds.
-        '''
-        
-        split_default = {"train" : [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 20, 22, "Y"], 
-                         "val" : [19, 21, "X"], 
-                         "test" : [7, 13]
-                        } # default split of data
-        
-        # Process string input for specific keys or fold names ("X", "Y")
-        if isinstance(split, str):
-            if split in ["X", "Y"]:
-                split = [split]
-            elif split in split_default:
-                split = split_default[split]
-            else:
-                raise ValueError(f"Invalid split value: {split}. Expected 'train', 'val', or 'test'.")
-
-        # int to list for unified processing
-        if isinstance(split, int):
-            split = [split]
-            
-        # Validate list of folds
-        if isinstance(split, list):
-            result = []
-            for item in split:
-                if isinstance(item, int) and 1 <= item <= 22:
-                    result.append(str(item))
-                elif isinstance(item, str) and item in ["X", "Y"]:
-                    result.append(item)
-                else:
-                    raise ValueError(f"Invalid fold value: {item}. Must be an integer in range 1-22 or 'X'/'Y'.")
-
-            split = result  # Ensure final result is clean and validated
-        
-        return split
