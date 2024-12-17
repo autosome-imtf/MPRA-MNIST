bdd77286a89b7ef8c27e9362e9c4dc20454e5e5b
Nikita P <nios.583@gamil.com>
2024-12-18 01:14:13 +0300

    small changes

--- mpramnist/info.py
+++ mpramnist/info.py
@@ -24,7 +24,6 @@ DEFAULT_ROOT = get_default_root()
 HOMEPAGE = "https://github.com/autosome-imtf/mpraMnist"
 
 INFO = {
-    "...": {},
     "VikramDataset": {
         "python_class": "VikramDataset",
         "description": "The VikramDataset is based on lentiMPRA assay, which determines the regulatory activity of over 680,000 sequences, representing a nearly comprehensive set of all annotated CREs among three cell types (HepG2, K562, and WTC11). HepG2 is a human liver cancer cell line, K562 is myelogenous leukemia cell line, WTC11 is pluripotent stem cell line derived from adult skin ",
@@ -38,6 +37,21 @@ INFO = {
         "scalar_features": {},
         "vector_features": {},
         "n_samples": {"train": 98336, "val": 12292, "test": 12292},
+        "folds": {"train": "1, 2, 3, 4, 5, 6, 7, 8", "val": 9, "test": 10}
+    },
+    "VikramJointDataset": {
+        "python_class": "VikramJointDataset",
+        "description": "",
+        "url_HepG2": "",
+        "MD5_HepG2": "",
+        "url_K562": "",
+        "MD5_K562": "",
+        "url_WTC11": "",
+        "MD5_WTC11": "",
+        "target_columns": {"HepG2","K562","WTC11"},
+        "scalar_features": {},
+        "vector_features": {},
+        "n_samples": {"train": 1, "val": 1, "test": 1},
         "folds": {"train": "1, 2, 3, 4, 5, 6, 7, 8", "val": 9, "test": 10}
     },
     "MalinoisDataset": {
