53965e47fa99e952cfb95948ca59c62a545316c1
Nikita P <nios.583@gmail.com>
2025-08-02 17:17:36 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ def readme():
     return content
 
 setup(name='mpramnist',
-      version="0.1.2",
-      description='available MPRA datasets',
-      url='https://github.com/autosome-imtf/MpraDataset',
-      author="Nikita P",
-      author_email='',
+      version="0.1.0",
+      description='standardized collection of MPRA datasets',
+      url='https://github.com/autosome-imtf/MPRA-MNIST',
+      author="Nikita Penzin",
+      author_email='nios.583@gmail.com',
       long_description=readme(),
       long_description_content_type="text/markdown",
       license='',
