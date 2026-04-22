40efc6d09aed748dbcd513c893b805340a81c3ec
Nikita P <nios.583@gmail.com>
2026-04-22 12:30:11 +0300

    small fix

--- mpramnist/Agarwal2025/__init__.py
+++ mpramnist/Agarwal2025/__init__.py
@@ -1,3 +1,4 @@
 from .dataset import AgarwalSingleDataset, AgarwalMultiDataset
+from .trainer import LitModel_AgarwalMulti, LitModel_AgarwalSingle
 
-__all__ = ['AgarwalSingleDataset', 'AgarwalMultiDataset']
+__all__ = ['AgarwalSingleDataset', 'AgarwalMultiDataset', 'LitModel_AgarwalSingle', 'LitModel_AgarwalMulti']
